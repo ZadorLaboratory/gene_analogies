caf1561a86635ba85a4aa909c7802abc0bc48c1f
aribenjamin <ari.s.benjamin@gmail.com>
2024-10-04 15:01:13 -0400

    embeddings now in the repo

--- data/dataloaders.py
+++ data/dataloaders.py
@@ -14,9 +14,14 @@ def load_geneformer_embeddings(path, reordered=True, brain=True):
     embeddings = np.array([embeddings_dict[gene] for gene in all_genes])
     return np.array(embeddings), list(all_genes)
 
-def load_word2vec_embeddings(path):
-    emb_path = os.path.join(path, "word2vec", "cleaned_google_news_embeddings.npy")
+def load_word2vec_embeddings(path, first_30k=True):
+    if first_30k:
+        emb_path = os.path.join(path, "word2vec", "google_news_embeddings_30k.npy")
+        words_path = os.path.join(path, "word2vec", "google_news_words_30k.npy")
+    else:
+        emb_path = os.path.join(path, "word2vec", "cleaned_google_news_embeddings.npy")
+        words_path = os.path.join(path, "word2vec", "cleaned_google_news_words.npy")
+
     embeddings= np.load(emb_path)
-    words_path = os.path.join(path, "word2vec", "cleaned_google_news_words.npy")
     words = np.load(words_path)
     return np.array(embeddings), list(words)
